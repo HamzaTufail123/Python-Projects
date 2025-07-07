635873e5ee595be35ab11a5ce66bd0fb77f6adda
Hamza Tufail <nayabtufail27@gmail.com>
2025-07-08 00:30:28 +0500

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,43 +1,80 @@
-feedback_list = []
-feedback_result = {}
-positive_feedback = 0
-negative_feedback = 0
-neutral_feedback = 0
+# user_num = int(input("Enter a number: "))
+# def func(user_num):
+#     factorial = 1
+#     for i in range(1, user_num + 1):
+#         factorial *= i
+#     return factorial
+# print(f"Factorial of {user_num} is: {func(user_num)}")
 
-user_feedback_range = int(input("Enter range of feedbacks: "))
 
-for i in range(user_feedback_range):
-    feedback = input(f"Enter your feedback #{i + 1}: ").strip()
-    feedback_list.append(feedback)
+# user_input = int(input("Enter a number: "))
+# def fabonachi(n):
+#     if n <= 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fabonachi(n - 1) + fabonachi(n - 2)
+# print(f"Fabonachi Series up to {fabonachi} terms.")
+# for i in range(user_input):
+#     print(fabonachi(i), end=" ")
 
-for j in feedback_list:
-    feedback_lower = j.lower()
-    if (
-        "excellent" in feedback_lower
-        or "good" in feedback_lower
-        or "great" in feedback_lower
-    ):
-        positive_feedback += 1
-        feedback_result[j] = "Positive"
-    elif (
-        "bad" in feedback_lower or "worst" in feedback_lower or "hard" in feedback_lower
-    ):
-        negative_feedback += 1
-        feedback_result[j] = "Negative"
+# *******************************************************
+account_data = {}
+transaction_log = []
+
+
+def create_account():
+    name = input("Enter your name: ")
+    balance = int(input("Enter your balance: "))
+    account_data["name"] = name
+    account_data["balance"] = balance
+    print(f"Welcome {name}, your account is created with Rs.{balance}")
+
+
+def deposit():
+    amount = int(input("Enter an amount: "))
+    account_data["balance"] += amount
+    transaction_log.append(f"Deposit  Rs.{amount}")
+
+
+def withdraw():
+    amount = int(input("Enter your withdraw amount: "))
+    if amount > account_data["balance"]:
+        print("Insufficient Funds.")
     else:
-        neutral_feedback += 1
-        feedback_result[j] = "Neutral"
+        account_data["balance"] -= amount
+        transaction_log.append(f"Withdraw Rs.{amount}")
 
-print("=" * 30)
 
-# Save to file
-with open("practice.txt", "w", encoding="utf-8") as file:
-    file.write("📊 Feedback Summary:\n")
-    file.write(f"Positive: {positive_feedback}\n")
-    file.write(f"Negative: {negative_feedback}\n")
-    file.write(f"Neutral: {neutral_feedback}\n")
-    file.write("\n📝 Feedback Details:\n")
-    for fb, sentiment in feedback_result.items():
-        file.write(f"• {fb} => {sentiment}\n")
+def view_balance():
+    print(f"Current Balance: Rs.{account_data['balance']}")
 
-print("✅ File 'practice.txt' saved successfully.")
+
+def save_history():
+    with open("practice.txt", "w", encoding="utf-8") as f:
+        f.write(f"User: {account_data['name']}\n")
+        f.write(f"Final Balance: {account_data['balance']}\n")
+        f.write("Transaction:\n")
+        for entry in transaction_log:
+            f.write(f"* {entry}\n")
+
+
+
+create_account()
+
+while True:
+    print("\n1. Deposit \n2. Withdraw \n3. Check balance \n4. Exit")
+    choice = input("Choose option: ")
+    if choice == "1":
+        deposit()
+    elif choice == "2":
+        withdraw()
+    elif choice == "3":
+        view_balance()
+    elif choice == "4":
+        save_history()
+        print("Thanks for using smart bank.")
+        break
+    else:
+        print("Invalid Option.")
